78f972b0d505128f67f4cb76579ded53b7b6425a
yaz-oss <ishimweyaziid749@gmail.com>
2026-05-18 04:19:07 +0200

    fixation

--- config/passport.js
+++ config/passport.js
@@ -24,7 +24,7 @@ passport.use(
           .GOOGLE_CLIENT_SECRET,
 
       callbackURL:
-        "https://yourbackend.onrender.com/auth/google/callback",
+       `${process.env.BACKEND_URL}/api/auth/google/callback`
 
     },
 
