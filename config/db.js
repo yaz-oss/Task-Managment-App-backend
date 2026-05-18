58bf51ee6523baad51dda8bf7970515aae89e8d4
yaz-oss <ishimweyaziid749@gmail.com>
2026-05-18 07:50:08 +0200

    db fix

--- config/db.js
+++ config/db.js
@@ -1,14 +1,24 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize } =
+require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  protocol: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+const sequelize =
+new Sequelize(
+  "postgresql://neondb_owner:npg_QtALO1YIDqg3@ep-late-sea-ab30awom-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  {
+    dialect: "postgres",
+
+    protocol: "postgres",
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-  },
-});
 
-module.exports = sequelize;
+    logging: false,
+  }
+);
+
+module.exports =
+sequelize;
